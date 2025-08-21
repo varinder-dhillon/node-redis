9b636a552304438b3d0e375ed4bc525bf852cdb3
New Name <new.email@example.com>
2025-08-22 00:14:43 +0530

    Added redis worker code

---
+++ worker/src/index.ts
@@ -0,0 +1,17 @@
+import { createClient } from  "redis";
+
+const client = createClient();
+
+async function main () {
+    await client.connect();
+
+    while(1){
+        const response = await client.brPop("submission", 0);
+
+        await new Promise((resolve, reject)=> setTimeout(resolve, 1000));
+
+        console.log("Processed users submissions!")
+    }
+}
+
+main();
